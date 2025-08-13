cafa410904d39b4686b2b25fbd6152e6a150b519
Lucasmenezes08 <lms4@cesar.school>
2025-08-13 09:00:57 -0300

    Adicionar exercicios.

---
+++ algoritmos/quickSort.ts
@@ -0,0 +1,26 @@
+const arr:number[] = [2,3,1,5,4,8,7,6,10];
+
+const quicksort = (array:number[]) => {
+
+    if (array.length < 2){
+        return array
+    }
+
+    let pivot:number = array[array.length - 1];
+    let right:number[] = [];
+    let left:number[] = [];
+
+    for (let i = 0 ; i < array.length - 1 ; i ++){
+        if (array[i] > pivot){
+            right.push(array[i]);
+        }
+
+        else{
+            left.push(array[i]);
+        }
+    }
+
+    return [...quicksort(left) , pivot , ...quicksort(right)];
+}
+
+console.log (quicksort(arr));
